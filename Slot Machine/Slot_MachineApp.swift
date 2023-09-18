23cb56a76e22cd90b1846482b33a7fd8838968a5
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-18 11:39:08 +0300

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Zeki Baklan on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
