8bbdbf4ce4c1953e19bca491a90d1e50b7c06c3f
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-20 13:53:29 +0300

    info commit

---
+++ Slot Machine/View/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by Zeki Baklan on 20.09.2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
